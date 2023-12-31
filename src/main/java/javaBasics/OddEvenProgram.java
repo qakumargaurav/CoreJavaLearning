375a06986c8ac2fce6fcf1e3ff2bf7caa291051b
dell <dell@dell-PC>
2023-12-31 07:57:16 +0530

    Odd even program

---
+++ src/main/java/javaBasics/OddEvenProgram.java
@@ -0,0 +1,28 @@
+package javaBasics;
+
+import java.util.Scanner;
+
+public class OddEvenProgram {
+
+	public static void main(String[] args) {
+
+		// Take input from user
+		Scanner sc = new Scanner(System.in);
+		// Ask user to input number
+		System.out.print("Enter your number:  ");
+		int number = sc.nextInt();
+		// Check given number is not a zero
+		if (number != 0) {
+			if (number % 2 == 0) {
+				System.out.println(number + "  is even number");
+			} else {
+				System.out.println(number + "  is odd number ");
+			}
+		} else {
+			System.out.println("Please enter valid number.");
+		}
+		sc.close();
+
+	}
+
+}
